3eeb7f26dc0c86b541f82ab49add184b79ef21c8
Wojciech Nowak <wojtekn00@gmail.com>
2022-11-22 01:09:25 +0100

    updated files with new features

--- src/components/ExpenseFilter/ExpensesFilter.js
+++ src/components/ExpenseFilter/ExpensesFilter.js
@@ -1,21 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Expenses from '../Expenses/Expenses';
 import './ExpensesFilter.css';
 
 const ExpensesFilter = () => {
+  const [year, setYear] = useState('');
+
+  const yearChangeHandler = (event) => {
+    setYear(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select onChange={yearChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
           <option value="2019">2019</option>
         </select>
+        {/* <span>{year}</span> */}
       </div>
     </div>
   );
 };
-
 export default ExpensesFilter;
