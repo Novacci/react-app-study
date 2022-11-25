a0ed06a8bf8fb356bee395f734ebff0b8d5ef018
Wojciech Nowak <wojtekn00@gmail.com>
2022-11-25 21:29:13 +0100

    changed Form and expense component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 function NewExpense(props) {
+  const [content, setContent] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseDate) => {
     const expenseData = {
       ...enteredExpenseDate,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setContent(false);
+  };
+
+  const contentHandler = () => {
+    setContent(true);
+  };
+
+  const stopContentHandler = () => {
+    setContent(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!content && <button onClick={contentHandler}>Add New Expense</button>}
+      {content && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopContentHandler}
+        />
+      )}
     </div>
   );
 }
